6a5e7f2ed0d2145bd88414ab53013b1ab96e3f97
prasanganath <sandun.wed@gmail.com>
2018-10-27 12:34:10 +0530

    new

--- add/src/App.js
+++ add/src/App.js
@@ -6,9 +6,13 @@ class App extends Component {
   render() {
     return (
         <div className="container">
+        <div className="row">
+            <div className="col-md-8">
         <form className="form-horizontal">
             <fieldset>
                 <legend>Search Nearby Places</legend>
+                <div class="col-md-10">
+
                 <div className="form-group">
                  <label htmlFor="InputEmail1" className="col-lg-2 control-label">Place</label>
                     <div className="col-lg-10">
@@ -16,8 +20,18 @@ class App extends Component {
 
                     </div>
                 </div>
+                </div>
+                <div className="col-md-2">
+                    <button className="btn btn-primary">Search</button>
+                </div>
             </fieldset>
         </form>
+            </div>
+            <div className="col-md-4">
+                <h2>Saved Places</h2>
+
+            </div>
+        </div>
         </div>
     );
   }
