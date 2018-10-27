d39994bc4a55bd96ce1de5df2021aae294c97108
prasanganath <sandun.wed@gmail.com>
2018-10-27 10:43:04 +0530

    update

--- add/src/App.js
+++ add/src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <form class="form-horizontal">
+            <fieldset>
+                <legend>Legend</legend>
+                <div class="form-group">
+                 <label for="InputEmail1" class="col-lg-2 control-label">Email</label>
+                    <div class="col-lg-10">
+                     <input type="text" class="form-control" id="inputEmail1" placeholder="Email"/>
+
+                    </div>
+                </div>
+            </fieldset>
+        </form>
     );
   }
 }
