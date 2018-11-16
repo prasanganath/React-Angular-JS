c62c0b31412ac00f8876e7abb2e123e6c0c58b52
prasanganath <sandun.wed@gmail.com>
2018-11-17 00:25:25 +0530

    update

--- sendemail/app.js
+++ sendemail/app.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 
 });
 app.post('/send', (req, res) => {
-    const output = '
+    const output = `
      <p>You have a new contct request</p>
      <h3>Contact Dtails</h3>
      <ul>
@@ -32,8 +32,48 @@ app.post('/send', (req, res) => {
          <li>Email: ${req.body.email}</li>
          <li>Phone: ${req.body.phone}</li>
      </ul>
+     <h3>Message</h3>
+     <p>${req.body.message}</p>
 
-    ';
+    `; 
+
+     // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'mail.traversymedia.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'sandun.wed@gmail.com', // generated ethereal user
+            pass: '123abc' // generated ethereal password
+        },
+        tls:{
+            rejectUnauthorzed
+        }
+
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Nodemailer Contact" <sandun.wed@gmail.com>', // sender address
+        to: 'gayansampathmanamendra@gmail.com', // list of receivers
+        subject: 'Node Contact Request', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.render('contact', {msg:'Email has been sent'});
+
+        
+    });
 });
 
 app.listen(3000, () => console.log('Server started...'));
