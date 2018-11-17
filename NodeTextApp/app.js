efd06528b986a79d57f5e66356ccefcf3f106932
prasanganath <sandun.wed@gmail.com>
2018-11-17 16:00:45 +0530

    node text app

---
+++ NodeTextApp/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
+
+//Init app
+
+const app = express();
