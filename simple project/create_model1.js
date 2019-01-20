5cc9a2097b0dfb56fef5d4a75fc6f106e30daace
prasanganath <sandun.wed@gmail.com>
2019-01-20 21:59:17 +0530

    server

---
+++ simple project/create_model1.js
@@ -0,0 +1,7 @@
+var mod=require('./createmodel2');
+var http= require('http');
+http.createServer(function(req,res){
+	res.end("Hello world");
+	
+}).listen(8080);
+console.log("node js server is listning");
---
+++ simple project/create_model2.js
@@ -0,0 +1,4 @@
+function func1(){
+	console.log('this is function 1');
+}
+module.express=func1;
