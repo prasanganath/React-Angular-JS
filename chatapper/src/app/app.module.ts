90ed1aa97c8b5d3104e75a3f53eb46857f855eec
prasanganath <sandun.wed@gmail.com>
2019-01-31 23:19:37 +0530

    add component

--- chatapper/src/app/app.module.ts
+++ chatapper/src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyAJb_DrCClC-a4g9mZ_lRhqDIWeG0Nbd4w",
+  authDomain: "chatapper-35689.firebaseapp.com",
+  databaseURL: "https://chatapper-35689.firebaseio.com",
+  projectId: "chatapper-35689",
+  storageBucket: "chatapper-35689.appspot.com",
+  messagingSenderId: "877863515300"
+};
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
