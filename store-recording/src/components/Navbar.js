c0cf1f967202e5607d2f9ee28296c081ccc7a1c5
prasanganath <sandun.wed@gmail.com>
2019-10-03 15:03:39 +0530

    add cart

--- store-recording/src/components/Navbar.js
+++ store-recording/src/components/Navbar.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'; 
+import logo from '../logo.svg'
+
 
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello Nav Bar</h3>
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                {
+               /* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk  */}
+                <Link to="/">
+                <img src={logo} alt="store"  className="navbar-brand"/>
 
-            </div>
-        )
-    }
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to="/" className="nav-link">
+                        products
+
+                        
+                    </Link>
+                </li>
+               
+
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                   <button>
+                   <i class="fas fa-cart-plus"></i>
+                   
+                       My Cart
+
+                    </button> 
+                </Link>
+                
+            </nav>
+        );
+    } 
 }
 
 
