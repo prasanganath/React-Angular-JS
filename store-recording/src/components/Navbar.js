1475695e8b6309353331189cb7b4cc0fdeafdcac
prasanganath <sandun.wed@gmail.com>
2019-10-03 16:18:35 +0530

    add styled

--- store-recording/src/components/Navbar.js
+++ store-recording/src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'; 
-import logo from '../logo.svg'
+import logo from "../logo.svg";
+import styled from 'styled-components'
+// import { transcode } from 'buffer';
+// import { transform } from '@babel/core';
+import { ButtonContainer } from "./Button";
 
 
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {
                /* 
 https://www.iconfinder.com/icons/1243689/call_phone_icon
@@ -29,17 +33,40 @@ https://www.iconfinder.com/Makoto_msk  */}
 
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                   <button>
-                   <i class="fas fa-cart-plus"></i>
+                   <ButtonContainer>
+                       <span className="mr-2">
+                       <i className="fas fa-cart-plus" />
+
+                       </span>
+                   
                    
                        My Cart
 
-                    </button> 
+                    </ButtonContainer> 
                 </Link>
                 
-            </nav>
+            </NavWrapper>
         );
     } 
 }
+
+const NavWrapper = styled.nav`
+background:var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    
+}
+
+
+
+`;
+
+
+
+
+
+
+
 
 
