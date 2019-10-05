c43713b0d8e27f246b86e00766b7e9e6967b6624
prasanganath <sandun.wed@gmail.com>
2019-10-05 06:11:53 +0530

    b

--- store-recording/src/components/Details.js
+++ store-recording/src/components/Details.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 //import Product from "./Product";
+import {ProductConsumer} from '../context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
-export default class ProductList extends Component {
+export default class Details extends Component {
     render() {
         return (
+            <ProductConsumer>
+               {value => {
+                  const {id,company,img,info,price,title,inCart}= value.detailProduct;
+                  return(
+                      <div className="container py-5">
+                      { /* title*/}
+                      <div className="row">
+                      <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
 
-            <div>
-              <h3>hello from details</h3>
+                      <h1>{title}</h1>
 
-            </div>
+                      </div>
 
+
+                      </div>
+                      { /* end title */}
+
+
+                      
+
+                      </div>
+                  )
+               }}
+            </ProductConsumer>
+
+            
         );
     }
 }
--- store-recording/src/context.js
+++ store-recording/src/context.js
@@ -9,27 +9,55 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
 
     state={
-        products:storeProducts,
-        detailProduct:detailProduct
+        products: [],
+        detailProduct: detailProduct
+
+    };
+    // setProducts = (){
+    //     let products = [];
+    //     storeProducts.forEach()
+    // }
+    componentDidMount(){
+        this.setProducts();
+         
+    }
+
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts,singleItem];
+
+
+        });
+        this.setState(()=>{
+            return {products:tempProducts}
+        });
 
     }
     handleDetail = () =>{
         console.log('hello from details');
-    }
+    };
     addToCart = () =>{
         console.log('hello from add to cart');
 
-    }
+    };
+    
+
+
+   
     render() {
         return (
-            <ProductContext.Provider value={{
+            <ProductContext.Provider 
+            value={{
 ...this.state,
-handleDetail:this.handleDetail,
-addToCart:this.addToCart,
+handleDetail: this.handleDetail,
+addToCart: this.addToCart
 
 
 
-            }}>
+            }}
+            >
                 {this.props.children}
 
 
