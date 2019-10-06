c9bb75c28f755e1cd0364bc7f2961faa28e5630e
prasanganath <sandun.wed@gmail.com>
2019-10-06 16:50:23 +0530

    add button

--- store-recording/src/components/Cart/CartItem.js
+++ store-recording/src/components/Cart/CartItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CartItem({item, value}) {
     const{id, title, img, price, total, count} = item;
-    const {increment, dercement, removeItem}= value
+    const {increment, decrement, removeItem}= value
     return (
        <div className="row my-1 text-capitalize text-center">
        <div className="col-10 mx-auto col-lg-2">
@@ -20,17 +20,34 @@ export default function CartItem({item, value}) {
        </div>
        
        <div className="col-10 mx-auto col-lg-2">
-        <span className="d-lg-none">product : </span>
+        <span className="d-lg-none">price : </span>
         {price}
        </div>
        <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
+        <div>
+            <span className="btn btn-black mx-1" onClick={()=>decrement(id)}
+            >
+                -
+            </span>
+            <span className="btn btn-black mx-1">{count}
 
+            </span>
+            <span className="btn btn-black mx-1" onClick={()=>increment(id)}
+            >
+                +
+            </span>
+
+        </div>
         </div>
 
        </div>
 
        { /* */}
+       <div className="col-10 mx-auto col-lg-2">
+       <strong>item total : $ {total}</strong>
+        {total}
+       </div>
        
 
        </div>
