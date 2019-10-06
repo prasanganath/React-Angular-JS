5639f9d78bcdbd45e0d68ac1a2e9c2bb7a4898b1
prasanganath <sandun.wed@gmail.com>
2019-10-06 15:28:03 +0530

    modify cart

--- store-recording/src/components/Cart/CartList.js
+++ store-recording/src/components/Cart/CartList.js
@@ -2,9 +2,15 @@ import React from "react";
 import CartItem from "./CartItem";
 
 
-export default function CartList(){
-    return(<div> hello from cart list
-        <CartItem></CartItem>
+export default function CartList({value}){
+    const {cart} = value;  
+    console.log(value, cart);
+    return(
+        <div className="container-fluid"> 
+            {cart.map(item=>{
+                return<CartItem key={item.id} item={item} value={value} />;
+            })}
+     
     </div>
     );
 }
