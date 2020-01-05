6cf1dbe47e63fce3ac8afa4b4bfabda933ff7ce9
prasanganath <sandun.wed@gmail.com>
2020-01-05 11:59:57 +0530

    save post module

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatSelectModule, MatButtonModule } from '@angular/material';
+//import { MatSelectModule, MatButtonModule } from '@angular/material';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, 
     AppRoutingModule,
-    MatSelectModule,
-    MatButtonModule,
+    // MatSelectModule,
+    // MatButtonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+
 
   ],
   providers: [],
