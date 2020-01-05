c13128f67871c4454549c53860dc3c21c24b49df
prasanganath <sandun.wed@gmail.com>
2020-01-05 14:22:19 +0530

    add new module

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 //import { MatSelectModule, MatButtonModule } from '@angular/material';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
+import {HeaderComponent} from './header/header.component';
+import  {PostListComponent} from './posts/post-list/post-list.component';
+
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatCardModule, MatButtonModule} from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule, 
@@ -22,7 +27,9 @@ import { MatInputModule, MatCardModule, MatButtonModule} from '@angular/material
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
 
 
   ],
