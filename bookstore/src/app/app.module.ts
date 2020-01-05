2b6636935ed5c9583aed8244496b915ecc4b90a4
prasanganath <sandun.wed@gmail.com>
2020-01-05 09:14:19 +0530

    first component

--- bookstore/src/app/app.module.ts
+++ bookstore/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatSelectModule, MatButtonModule } from '@angular/material';
+import {PostCreateComponent} from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSelectModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
